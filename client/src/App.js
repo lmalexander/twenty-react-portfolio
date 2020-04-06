f563bc6473ba98fa156e80960c9a41c238313367
lmalexander <elizabeth.alexander@northwestern.edu>
2020-04-05 22:37:27 -0500

    wrote router in app.js, NoMatch component, copied react-bootstrap Nav component

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import Academic from "./pages/Academic";
+import WebDev from "./pages/WebDev";
+import MultiMedia from "./pages/MultiMedia";
+import Contact from "./pages/Contact";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/academic" component={Academic} />
+          <Route exact path="/webdev" component={WebDev} />
+          <Route exact path="/multimedia" component={MultiMedia} />
+          <Route exact path="/contact" component={Contact} />
+          <Route component={NoMatch} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
